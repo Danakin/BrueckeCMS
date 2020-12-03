bf350d8fb1fc74b23648e0739f59e91b2a4ca38f
Danny Festor <danny@festor.info>
2020-12-03 09:57:49 +0900

    Update User Index View

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,17 +1,27 @@
 @extends('admin.layouts.app')
 
 @section('content')
-<ul>
+<section class="w-full p-4 rounded bg-white divide-y-2 divide-gray-600">
+    <article class="flex flex-row flex-wrap">
+        <div class="w-full sm:w-3/12 font-bold">Email</div>
+        <div class="w-full sm:w-3/12 font-bold">Name</div>
+        <div class="w-full sm:w-6/12 font-bold">Roles</div>
+    </article>
     @foreach($users as $user)
-    <li>
-        {{ $user->email }}
-        <ul>
+    <article class="flex flex-row flex-wrap my-2">
+        <div class="w-full sm:w-3/12 font-bold">
+            <a href="{{ route('admin.users.edit', $user) }}">{{ $user->email }}</a>
+        </div>
+        <div class="w-full sm:w-3/12">
+            <a href="{{ route('admin.users.edit', $user) }}">{{ $user->name }}</a>
+        </div>
+        <div class="w-full sm:w-6/12">
             @foreach($user->roles as $role)
-            <li><a href="{{ route('admin.roles.index') }}">{{ $role->name }}</a></li>
+            <a href="{{ route('admin.roles.edit', $role) }}">{{ $role->name }}</a>
             @endforeach
-        </ul>
-    </li>
+        </div>
+    </article>
     @endforeach
-</ul>
-
+    </>
+</section>
 @endsection
