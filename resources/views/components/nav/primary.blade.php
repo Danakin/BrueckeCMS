31757d4705521802ac6a03da4939072ba9bce013
Danny Festor <danny@festor.info>
2020-12-15 10:45:02 +0900

    Edit Primary Menu

--- resources/views/components/nav/primary.blade.php
+++ resources/views/components/nav/primary.blade.php
@@ -17,11 +17,20 @@
     <section class="flex">
         @foreach($menu->items as $item)
         @if ($item->post_type_id)
-        <a class="px-4 flex items-center bg-blue-200 hover:bg-blue-400"
-            href="{{ $item->type->prefix }}">{{ $item->title }}</a>
-        @else
-        <a class="px-4 flex items-center bg-blue-200 hover:bg-blue-400" href="{{ $item->uri }}">{{ $item->title }}</a>
-        @endif
-        @endforeach
+        <a class="px-4 flex items-center bg-blue-200 hover:bg-blue-400" href="{{ $item->type->prefix }}">
+            @else
+            <a class="px-4 flex items-center bg-blue-200 hover:bg-blue-400" href="{{ $item->uri }}">
+                @endif
+                @if ($item->title)
+                {{ $item->title }}
+                @elseif ($item->posty_type_id && $item->type->name)
+                {{ $item->type->name }}
+                @elseif ($item->uri)
+                {{ $item->uri }}
+                @else
+                {{ $item->id }}
+                @endif
+            </a>
+            @endforeach
     </section>
 </nav>
