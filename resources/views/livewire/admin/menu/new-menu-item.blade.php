3a0da386e403ebe5ce71dbf1476e0bb7c4f25e09
Danny Festor <danny.festor@gmail.com>
2021-01-15 13:59:04 +0900

    Use Components For Inputs in New Menu Item

--- resources/views/livewire/admin/menu/new-menu-item.blade.php
+++ resources/views/livewire/admin/menu/new-menu-item.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ dialog: false, title: '', uri: '' }">New Fixed Link
+<div x-data="{ title: @entangle('title'), uri: @entangle('uri'), dialog: false }">New Fixed Link
     <div class="w-full hover:bg-blue-600 cursor-pointer py-2 bg-blue-400 rounded text-center text-white flex justify-center items-center"
         @click="dialog = true">
         New Fixed Link
@@ -6,12 +6,11 @@
     <div class="absolute top-0 bottom-0 left-0 right-0 bg-gray-200 z-10 flex justify-center align-center"
         :class="{ 'hidden' : !dialog }" @click.stop="dialog = false">
         <form wire:submit.prevent="" @click.stop="" class="flex flex-col text-black p-8">
-            <label for="title">Title</label>
-            <input type="text" name="title" x-model="title">
-            <label for="uri">URI</label>
-            <input type="text" name="uri" x-model="uri">
+            <x-input.text name="title" model="title">Title</x-input.text>
+            <x-input.text name="uri" model="uri">URI</x-input.text>
+            
             <button type="submit"
-                @click.stop="$wire.createMenuItem(title, uri); title=''; uri=''; dialog=false">Submit</button>
+                @click.stop="$wire.createMenuItem(); dialog=false">Submit</button>
         </form>
     </div>
 </div>
