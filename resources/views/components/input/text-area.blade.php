485e34842390fbf4d72b37e0ce17d4cf01929b6b
Danny Festor <danny.festor@gmail.com>
2021-01-15 13:58:50 +0900

    Debounce Inputs a bit more

--- resources/views/components/input/text-area.blade.php
+++ resources/views/components/input/text-area.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-row flex-wrap mb-2">
     <label class="w-full sm:w-3/12" for="{{ $name }}">{{ $label }}</label>
-    <textarea name="{{ $name }}" id="{{ $name }}" cols="30" rows="3" wire:model="{{ $model }}"
+    <textarea name="{{ $name }}" id="{{ $name }}" cols="30" rows="3" wire:model.debounce.500ms="{{ $model }}"
         class="w-full sm:w-9/12"></textarea>
     @include('components.input.partials.error', ['name' => $name])
 </div>
--- resources/views/components/input/text.blade.php
+++ resources/views/components/input/text.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-row flex-wrap mb-2">
     <label class="w-full sm:w-3/12" for="{{ $name }}">{{ $slot }}</label>
     <input class="w-full sm:w-9/12" type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" value="{{ $value }}"
-        wire:model="{{ $model }}" />
+        wire:model.debounce.500ms="{{ $model }}" />
     @include('components.input.partials.error', ['name' => $name])
 </div>
