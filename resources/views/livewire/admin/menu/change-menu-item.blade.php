cf52b2d2df2a0e901d22468c38ac8cc5b807ee48
Danny Festor <danny.festor@gmail.com>
2021-01-15 13:02:39 +0900

    Change nav bug

--- resources/views/livewire/admin/menu/change-menu-item.blade.php
+++ resources/views/livewire/admin/menu/change-menu-item.blade.php
@@ -1,25 +1,31 @@
 <section class="grid grid-cols-2 relative">
     <ul>
+        <li class="font-bold my-3 px-4">Available</li>
         @foreach ($posttypes as $item)
             <li
                 wire:key="{{ $item['id'] }}"
-                class="border border-gray-400 px-4 py-2 cursor-pointer rounded m-2"
+                class="border border-gray-400 px-4 py-2 cursor-pointer rounded m-2 flex justify-between"
                 wire:click="addToMenu({{ $item }})"
                 animate-move
             >
-                {{ $item['name'] }}
+                <div>
+                    {{ $item['name'] }}
+                </div>
+                <div>➡</div>
             </li>
         @endforeach
     </ul>
     <ul>
+        <li class="font-bold my-3 px-4">In menu</li>
         @foreach ($items as $item)
             <li
                 wire:key="{{ $item['id'] }}"
-                class="border border-gray-400 px-4 py-2 cursor-pointer rounded m-2"
+                class="border border-gray-400 px-4 py-2 cursor-pointer rounded m-2 flex justify-between"
                 wire:click="removeFromMenu({{ $item }})"
                 animate-move
             >
-                {{ $item['title'] }}
+                <div>⬅</div>
+                <div>{{ $item['title'] }}</div>
             </li>
         @endforeach
     </ul>
