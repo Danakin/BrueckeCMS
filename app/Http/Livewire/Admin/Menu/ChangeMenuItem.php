7c85428504a41f83c77cbbccbdc8e6edc39eba5a
Danny Festor <danny.festor@gmail.com>
2021-01-15 11:12:18 +0900

    Better Add and Remove Menu Items

--- app/Http/Livewire/Admin/Menu/ChangeMenuItem.php
+++ app/Http/Livewire/Admin/Menu/ChangeMenuItem.php
@@ -4,50 +4,58 @@ namespace App\Http\Livewire\Admin\Menu;
 
 use App\Models\PostType;
 use App\Models\MenuItem;
+use Illuminate\Support\Arr;
 use Livewire\Component;
 
 class ChangeMenuItem extends Component
 {
-    public $posttypes = [];
-    public $items = [];
+    public $posttypes;
+    public $items;
     public $menu;
     public $menu_ids;
     public $title;
     public $post_type_id;
 
-    protected $listeners = ['menuItemChanged' => 'updateComponents'];
+    protected $listeners = ["menuItemChanged" => "updateComponents"];
 
-    public function mount($menu) {
+    public function mount($menu)
+    {
         $this->menu = $menu;
-        $items = $menu->items;
         // TODO UPDATE POSTTYPE WITH name->title prefix->uri
         $this->items = $menu->items;
-        $this->menu_ids = array_filter($this->items->pluck('post_type_id')->toArray());
-        $this->posttypes = PostType::whereNotIn('id', $this->menu_ids)->get();
+        $this->menu_ids = array_filter(
+            $this->items->pluck("post_type_id")->toArray()
+        );
+        $this->posttypes = PostType::whereNotIn("id", $this->menu_ids)->get();
     }
 
-    public function addToMenu($title, $id) {
-        MenuItem::create([
+    public function addToMenu($item)
+    {
+        $this->posttypes = $this->posttypes->filter(function ($v) use ($item) {
+            return $v->id !== $item["id"];
+        });
+
+        $newItem = MenuItem::firstOrCreate([
             "menu_id" => $this->menu->id,
-            "post_type_id" => $id,
-            "title" => $title,
+            "post_type_id" => $item["id"],
+            "title" => $item["name"],
         ]);
-        $this->emit('menuItemChanged');
+
+        Arr::add($this->items, count($this->items), $newItem);
     }
 
-    public function removeFromMenu($item) {
-        MenuItem::where('id', $item)->delete();
-        $this->emit('menuItemChanged');
-    }
-
-    public function updateComponents() {
-        $this->items = $this->menu->items;
-        $this->menu_ids = array_filter($this->items->pluck('post_type_id')->toArray());
-        $this->posttypes = PostType::whereNotIn('id', $this->menu_ids)->get();
+    public function removeFromMenu($item)
+    {
+        $menuItem = MenuItem::find($item["id"]);
+        $this->items = $this->items->filter(function ($v) use ($item) {
+            return $v->id !== $item["id"];
+        });
+        Arr::add($this->posttypes, count($this->posttypes), $menuItem->type);
+        $menuItem->delete();
     }
 
     public function render()
     {
-        return view('livewire.admin.menu.change-menu-item');
+        return view("livewire.admin.menu.change-menu-item");
     }
 }
--- resources/views/livewire/admin/menu/change-menu-item.blade.php
+++ resources/views/livewire/admin/menu/change-menu-item.blade.php
@@ -1,43 +1,29 @@
-<section class="flex flex-col md:flex-row relative">
-    <article class="flex flex-col w-full md:w-6/12">
-        @foreach($posttypes as $index => $type)
-        <div x-data="{ dialog: false, title: '', id: '' }">
-            <div class="flex flex-row hover:bg-gray-200 p-4 cursor-pointer relative"
-                @click="dialog = true; title = '{{ $type->name }}'; id = '{{ $type->id }}'" wire:key="{{ $type->id }}">
-                <div class="flex-1">
-                    {{ $type->name }}
-                </div>
-                <div class="w-1/12">
-                    ➡
-                </div>
-            </div>
-            <div class="absolute top-0 left-0 right-0 bottom-0 bg-gray-200 z-20 flex justify-center items-center"
-                :class="{ 'hidden' : !dialog }">
-                <form wire:submit.prevent="" @click.prevent="">
-                    <label for="title">Title</label>
-                    <input type="text" id="title" name="title" x-model="title">
-                    <button type="submit"
-                        @click="$wire.addToMenu(title, id); dialog = false; title = null; id = null">Submit</button>
-                </form>
-            </div>
-        </div>
+<section class="grid grid-cols-2 relative">
+    <ul>
+        @foreach ($posttypes as $item)
+            <li
+                wire:key="{{ $item['id'] }}"
+                class="border border-gray-400 px-4 py-2 cursor-pointer rounded m-2"
+                wire:click="addToMenu({{ $item }})"
+            >
+                {{ $item['name'] }}
+            </li>
         @endforeach
-    </article>
-    <article class="flex flex-col w-full md:w-6/12">
-        @foreach($items as $index => $item)
-        <div wire:click="removeFromMenu({{ $item->id }})" wire:key="{{ $item->id }}"
-            class="flex flex-row hover:bg-gray-200 p-4 cursor-pointer">
-            <div class="w-1/12">⬅</div>
-            <div class="flex-1">
-                @if ($item->title && strlen($item->title) > 0)
-                {{ $item->title }}
-                @elseif ($item->post_type_id && $item->post_type_id > 0 && $item->type)
-                {{ $item->type->name }}
-                @else
-                {{ $item->id }}
-                @endif
-            </div>
-        </div>
+    </ul>
+    <ul>
+        @foreach ($items as $item)
+            <li
+                wire:key="{{ $item['id'] }}"
+                class="border border-gray-400 px-4 py-2 cursor-pointer rounded m-2"
+                wire:click="removeFromMenu({{ $item }})"
+            >
+                {{ $item['title'] }}
+            </li>
         @endforeach
-    </article>
+    </ul>
+    <div x-html="$wire.items['0']"></div>
 </section>
+
+<script>
+    console.log(Livewire);
+</script>
